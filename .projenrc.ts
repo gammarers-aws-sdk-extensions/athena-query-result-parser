e2cc8c3a4424c2aefb878b6c8b80158924d4991f
yicr <yicr@users.noreply.github.com>
2026-07-28 10:25:48 +0000

    chore: switch package manager from yarn to npm

--- .projenrc.ts
+++ .projenrc.ts
@@ -6,7 +6,7 @@ const project = new typescript.TypeScriptProject({
   name: 'athena-query-result-parser',
   projenrcTs: true,
   typescriptVersion: '6.0.x',
-  packageManager: javascript.NodePackageManager.YARN_CLASSIC,
+  packageManager: javascript.NodePackageManager.NPM,
   repository: 'https://github.com/gammarers-aws-sdk-extensions/athena-query-result-parser.git',
   description: 'A small TypeScript library that parses Amazon Athena query result `ResultSet` objects (from `@aws-sdk/client-athena`) into header-based row objects. It handles metadata-driven headers, skips the header row when present, and supports custom row transformers.',
   deps: [
